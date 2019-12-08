ca271c588329edc628a37a5fb831b89ac2e7f134
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2019-12-09 05:18:01 +0700

    search

--- routes/index.js
+++ routes/index.js
@@ -50,9 +50,171 @@ router.get('/', function(req, res, next) {
   res.render('index',{dat1});
 });
 
+var dat2 = [
+  {
+      "name":"G_SHOCK",
+      "seri":"GAS-100BL-1A",
+      "des":"Sắc đen cơ bản của mẫu này được cập nhật thêm các điểm nhấn màu tím như ánh sáng đèn neon trên gờ và mặt đồng hồ để tạo vẻ ngoài ít sành công nghệ",
+      "price":"450.000 VNĐ",
+      "oldprice":"500.000 VNĐ",
+      "url":"images/shop_product/01.jpg"
+  },
+  {
+      "name":"G_SHOCK",
+      "seri":"DW-5600THB-7",
+      "des":"Không có mô tả",
+      "price":"350.000 VNĐ",
+      "oldprice":"400.000 VNĐ",
+      "url":"images/shop_product/02.jpg"
+    },
+    {
+        "name":"Orient",
+        "seri":"SAB0B007WB Nam",
+        "des":"Không có mô tả",
+        "price":"4.610.000 VNĐ",
+        "oldprice":"4.990.000 VNĐ",
+        "url":"images/shop_product/03.jpg"
+      },
+      {
+            "name":"G_SHOCK",
+            "seri":"GAS-100BL-1A",
+            "des":"Sắc đen cơ bản của mẫu này được cập nhật thêm các điểm nhấn màu tím như ánh sáng đèn neon trên gờ và mặt đồng hồ để tạo vẻ ngoài ít sành công nghệ",
+            "price":"450.000 VNĐ",
+            "oldprice":"500.000 VNĐ",
+            "url":"images/shop_product/01.jpg"
+        },
+        {
+          "name":"G_SHOCK",
+          "seri":"DW-5600THB-7",
+          "des":"Không có mô tả",
+          "price":"350.000 VNĐ",
+          "oldprice":"400.000 VNĐ",
+          "url":"images/shop_product/02.jpg"
+        },
+        {
+            "name":"Orient",
+            "seri":"SAB0B007WB Nam",
+            "des":"Không có mô tả",
+            "price":"4.610.000 VNĐ",
+            "oldprice":"4.990.000 VNĐ",
+            "url":"images/shop_product/03.jpg"
+         },
+      {
+            "name":"G_SHOCK",
+            "seri":"GAS-100BL-1A",
+            "des":"Sắc đen cơ bản của mẫu này được cập nhật thêm các điểm nhấn màu tím như ánh sáng đèn neon trên gờ và mặt đồng hồ để tạo vẻ ngoài ít sành công nghệ",
+            "price":"450.000 VNĐ",
+            "oldprice":"500.000 VNĐ",
+            "url":"images/shop_product/01.jpg"
+        },
+        {
+          "name":"G_SHOCK",
+          "seri":"DW-5600THB-7",
+          "des":"Không có mô tả",
+          "price":"350.000 VNĐ",
+          "oldprice":"400.000 VNĐ",
+          "url":"images/shop_product/02.jpg"
+        },
+        {
+            "name":"Orient",
+            "seri":"SAB0B007WB Nam",
+            "des":"Không có mô tả",
+            "price":"4.610.000 VNĐ",
+            "oldprice":"4.990.000 VNĐ",
+            "url":"images/shop_product/03.jpg"
+          },
+      {
+            "name":"G_SHOCK",
+            "seri":"GAS-100BL-1A",
+            "des":"Sắc đen cơ bản của mẫu này được cập nhật thêm các điểm nhấn màu tím như ánh sáng đèn neon trên gờ và mặt đồng hồ để tạo vẻ ngoài ít sành công nghệ",
+            "price":"450.000 VNĐ",
+            "oldprice":"500.000 VNĐ",
+            "url":"images/shop_product/01.jpg"
+        },
+        {
+          "name":"G_SHOCK",
+          "seri":"DW-5600THB-7",
+          "des":"Không có mô tả",
+          "price":"350.000 VNĐ",
+          "oldprice":"400.000 VNĐ",
+          "url":"images/shop_product/02.jpg"
+        },
+        {
+            "name":"Orient",
+            "seri":"SAB0B007WB Nam",
+            "des":"Không có mô tả",
+            "price":"4.610.000 VNĐ",
+            "oldprice":"4.990.000 VNĐ",
+            "url":"images/shop_product/03.jpg"
+          },
+      {
+            "name":"Orient",
+            "seri":"SAB0B007WB Nam",
+            "des":"Không có mô tả",
+            "price":"4.610.000 VNĐ",
+            "oldprice":"4.990.000 VNĐ",
+            "url":"images/shop_product/03.jpg"
+      },
+      {
+            "name":"Orient",
+            "seri":"SAB0B007WB Nam",
+            "des":"Không có mô tả",
+            "price":"4.610.000 VNĐ",
+            "oldprice":"4.990.000 VNĐ",
+            "url":"images/shop_product/03.jpg"
+      },
+      {
+                  "name":"Orient",
+                  "seri":"SAB0B007WB Nam",
+                  "des":"Không có mô tả",
+                  "price":"4.610.000 VNĐ",
+                  "oldprice":"4.990.000 VNĐ",
+                  "url":"images/shop_product/03.jpg"
+                },
+            {
+                  "name":"G_SHOCK",
+                  "seri":"GAS-100BL-1A",
+                  "des":"Sắc đen cơ bản của mẫu này được cập nhật thêm các điểm nhấn màu tím như ánh sáng đèn neon trên gờ và mặt đồng hồ để tạo vẻ ngoài ít sành công nghệ",
+                  "price":"450.000 VNĐ",
+                  "oldprice":"500.000 VNĐ",
+                  "url":"images/shop_product/01.jpg"
+              },
+              {
+                "name":"G_SHOCK",
+                "seri":"DW-5600THB-7",
+                "des":"Không có mô tả",
+                "price":"350.000 VNĐ",
+                "oldprice":"400.000 VNĐ",
+                "url":"images/shop_product/02.jpg"
+              },
+              {
+                  "name":"Orient",
+                  "seri":"SAB0B007WB Nam",
+                  "des":"Không có mô tả",
+                  "price":"4.610.000 VNĐ",
+                  "oldprice":"4.990.000 VNĐ",
+                  "url":"images/shop_product/03.jpg"
+                },
+            {
+                  "name":"Orient",
+                  "seri":"SAB0B007WB Nam",
+                  "des":"Không có mô tả",
+                  "price":"4.610.000 VNĐ",
+                  "oldprice":"4.990.000 VNĐ",
+                  "url":"images/shop_product/03.jpg"
+            },
+            {
+                  "name":"Orient",
+                  "seri":"SAB0B007WB Nam",
+                  "des":"Không có mô tả",
+                  "price":"4.610.000 VNĐ",
+                  "oldprice":"4.990.000 VNĐ",
+                  "url":"images/shop_product/03.jpg"
+            },
+]
 
 router.get('/search', function(req, res, next) {
-  res.render('search');
+  res.render('search',{dat1,dat2});
 });
 
 module.exports = router;
